c99faca1e9308ad590de1bf35d2c04f76a4ee74e
Enric <enricvbufi@gmail.com>
2025-01-21 17:11:20 +0100

    Added restaurant.table with fields and constraints

---
+++ models/table.py
@@ -0,0 +1,16 @@
+from odoo import models, fields, api
+
+
+class Table(models.Model):
+    _name = 'restaurant.table'
+    _description = 'restaurant.table'
+
+    tableNumber = fields.Integer(string="Table identify number", required=True, unique=True)
+    local = fields.Many2one('restaurant.local', string="Local which this table belongs", required=True)
+    positionX = fields.Integer(string="X Position for visual purposes", required=True)
+    positionY = fields.Integer(string="Y Position for visual purposes", required=True)
+    chairs = fields.Integer(string="Number of chairs per table", required=True)
+
+    _sql_constraints = [
+        ('table_number_local_unique', 'UNIQUE(tableNumber, local)', 'There can be just one same table number per local.')
+    ]
