6bb98fa84dadedfdb404d9a07de8bc8d6e1aa4d5
Cesar <cesarabadvel@gmail.com>
2025-02-12 20:11:58 +0100

    Table changes

--- models/table.py
+++ models/table.py
@@ -7,8 +7,6 @@ class Table(models.Model):
 
     tableNumber = fields.Integer(string="Table identify number", required=True, unique=True)
     local = fields.Many2one('restaurant.local', string="Local which this table belongs", required=True)
-    positionX = fields.Integer(string="X Position for visual purposes", required=True)
-    positionY = fields.Integer(string="Y Position for visual purposes", required=True)
     chairs = fields.Integer(string="Number of chairs per table", required=True)
 
     book = fields.One2many(comodel_name = "restaurant.book", string = "Books of this table", inverse_name="table")
