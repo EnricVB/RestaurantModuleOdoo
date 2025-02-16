f985e621de35181c4802d213689085ceddec0f0d
Cesar Abad <cesarabadvel@gmail.com>
2025-02-16 20:54:14 +0100

    Table kanban

--- models/table.py
+++ models/table.py
@@ -1,5 +1,5 @@
 from odoo import models, fields, api
-
+from datetime import datetime, timedelta
 
 class Table(models.Model):
     _name = 'restaurant.table'
@@ -10,6 +10,23 @@ class Table(models.Model):
     chairs = fields.Integer(string="Number of chairs per table", required=True)
 
     book = fields.One2many(comodel_name = "restaurant.book", string = "Books of this table", inverse_name="table")
+    bookTableOccupancy = fields.Integer(string = "Table occupancy at actual book", compute = "_ocuppancyPerDate")
+
+
+    @api.depends('book.date','book.tableOcupancy')
+    def _ocuppancyPerDate(self):
+        now = datetime.now()
+        for table in self:
+            if not table.book:  # Si no hay reservas, asignar 0
+                table.bookTableOccupancy = 0
+                continue
+
+            # Filtramos las reservas activas (en la última hora)
+            active_books = table.book.filtered(lambda b: b.date and (b.date <= now <= (b.date + timedelta(hours=1))))
+            
+            table.bookTableOccupancy = active_books[0].tableOcupancy if active_books else 0  # Primera reserva activa o 0
+
+    
 
     _sql_constraints = [
         ('table_number_local_unique', 'UNIQUE(tableNumber, local)', 'There can be just one same table number per local.')
